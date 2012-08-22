c7aaa1b99069f8b9246026a5d461341d4586446f
Kamil Grzebien <ciptok@gmail.com>
2012-08-22 20:35:18 +0100

    Pull all changes from nezticle repo

--- package/raspberrypi/VideoCore/VideoCore.mk
+++ package/raspberrypi/VideoCore/VideoCore.mk
@@ -1,6 +1,6 @@
-VIDEOCORE_VERSION = master
-VIDEOCORE_SITE = git://github.com/raspberrypi/firmware.git
-VIDEOCORE_SITE_METHOD = git
+VIDEOCORE_VERSION = 7680bb3
+VIDEOCORE_SITE = http://bsquask.com/downloads/firmware
+VIDEOCORE_SOURCE = raspberrypi-firmware-$(VIDEOCORE_VERSION).tar.gz
 VIDEOCORE_INSTALL_STAGING = YES
 VIDEOCORE_INSTALL_TARGET = YES
 
@@ -13,6 +13,8 @@ define VIDEOCORE_INSTALL_TARGET_CMDS
 	cp -r $(@D)/hardfp/opt/vc/bin $(TARGET_DIR)/opt/vc
 	cp -r $(@D)/hardfp/opt/vc/lib $(TARGET_DIR)/opt/vc
 	cp -r $(@D)/hardfp/opt/vc/sbin $(TARGET_DIR)/opt/vc
+	# add /opt/vc/lib to default library path
+	echo /opt/vc/lib >> $(TARGET_DIR)/etc/ld.so.conf
 endef
 
 $(eval $(call GENTARGETS))
