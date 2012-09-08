157306fa05344722f8421392da00f20f5e79293f
Kamil Grzebien <ciptok@gmail.com>
2012-09-08 11:17:01 +0100

    Merge latest changes from nezticle branch

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -8,6 +8,9 @@ echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=
 echo "disable_overscan=1" > $TARGETDIR/boot/config.txt
 echo "arm_freq=800" >> $TARGETDIR/boot/config.txt
 
+# Point /bin/sh to /bin/bash
+ln -T -s /bin/bash $TARGETDIR/bin/sh 
+
 # Package the /boot partition
 tar -czf $TARGETDIR/../images/boot.tar.gz --exclude=Image -C $TARGETDIR/boot/ .
 
